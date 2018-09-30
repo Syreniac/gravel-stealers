5ed8d38500abf5a2fdd62c52998f4d1d62c89e53
Syreniac <SyreniacTEST@gmail.com>
2018-09-30 23:54:01 +0100

    Expand RTS camera to support internal empty space

--- Assets/Scripts/CameraRoot.cs
+++ Assets/Scripts/CameraRoot.cs
@@ -3,33 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Instantiation : MonoBehaviour {
+public class CameraRoot : MonoBehaviour {
+
+	private enum CameraBehavior
+	{
+		RTS,
+		SIDE_N,
+		SIDE_E,
+		SIDE_S,
+		SIDE_W
+	}
 
 	private const float CAMERA_SPEED_MULTI = 2.0f;
 
+	private const int X_DIM = 25, Y_DIM = 25, Z_DIM = 5;
+
 	private const float MIN_Y = 0.0f, MIN_X = 0.0f, MIN_Z = 0.0f;
 
-	private const float MAX_Y = 5.0f, MAX_X = 5.0f, MAX_Z = 5.0f;
+	private const float MAX_Y = Y_DIM, MAX_X = X_DIM, MAX_Z = Z_DIM - 0.1f;
 
 	private int blockX, blockY, blockZ;
 
-	public Transform block;
+	private CameraBehavior cameraBehavior;
 
-	private GameObject[,,] blocks = new GameObject[5,5,5]; 
+	public GameObject block;
+
+	private GameObject[,,] blocks = new GameObject[X_DIM,Y_DIM,Z_DIM];
 	
+
 	void Start () {
-		// Create a 5x5x5 block of cubes
-		for (int x = 0; x < 5; x++)
+		// Create a XxYxZ block of cubes
+		for (int x = 0; x < X_DIM; x++)
 		{
-			for (int y = 0; y < 5; y++)
+			for (int y = 0; y < Y_DIM; y++)
 			{
-				for (int z = 0; z < 5; z++)
+				blocks[x, y, 0] = Instantiate(block, new Vector3(x, y, 0), Quaternion.identity);
+				if ((x == X_DIM - 1 || x == 0) || (y == Y_DIM - 1 || y == 0))
 				{
-					Transform temp = Instantiate(block, new Vector3(x, y, z), Quaternion.identity);
-					blocks[x, y, z] = temp.gameObject;
+					for (int z = 1; z < Z_DIM - 1; z++)
+					{
+						blocks[x, y, z] = Instantiate(block, new Vector3(x, y, z), Quaternion.identity);
+					}
 				}
+				blocks[x, y, Z_DIM - 1] = Instantiate(block, new Vector3(x, y, Z_DIM - 1), Quaternion.identity);
 			}
 		}
+		
 		blockX = 0;
 		blockY = 0;
 		blockZ = 0;
@@ -94,6 +113,7 @@ public class Instantiation : MonoBehaviour {
 		int newBlockZ = (int) position.z;
 		if (newBlockY != blockY || newBlockX != blockX || newBlockZ != blockZ)
 		{
+			Debug.Log(newBlockX+", "+newBlockY+", "+newBlockZ);
 			blockX = newBlockX;
 			blockY = newBlockY;
 			blockZ = newBlockZ;
@@ -105,19 +125,29 @@ public class Instantiation : MonoBehaviour {
 
 	private void changeHiddenBlocks()
 	{
-		for (int x = 0; x < 5; x++)
+		switch (cameraBehavior)
 		{
-			for (int y = 0; y < 5; y++)
-			{
-				for (int z = 0; z < blockZ; z++)
+			case CameraBehavior.RTS:
+				for (int x = 0; x < X_DIM; x++)
 				{
-					blocks[x, y, z].GetComponent<MeshRenderer>().enabled = false;
+					for (int y = 0; y < Y_DIM; y++)
+					{
+						if (blocks[x, y, 0] != null) {
+							blocks[x, y, 0].GetComponent<MeshRenderer>().enabled = (0 >= blockZ);
+						}
+						for (int z = 1; z < Z_DIM; z++)
+						{
+							if (blocks[x, y, z] != null)
+							{
+								blocks[x, y, z].GetComponent<MeshRenderer>().enabled = (z >= blockZ) || !(blocks[x, y, z - 1] == null || !blocks[x,y,z - 1].GetComponent<MeshRenderer>().enabled) ;
+							}
+						}
+					}
 				}
-				for (int z = blockZ; z < 5; z++)
-				{
-					blocks[x, y, z].GetComponent<MeshRenderer>().enabled = true;
-				}
-			}
+				break;
+			case CameraBehavior.SIDE_N:
+
+				break;
 		}
 	}
 }
